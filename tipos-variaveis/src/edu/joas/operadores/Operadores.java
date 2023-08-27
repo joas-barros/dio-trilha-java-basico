bd34dcdb14d7ef13f793e8e1ce4a28795fc07156
joas-barros <joas.barros14@hotmail.com>
2023-08-27 10:03:25 -0300

    Criando pacotes e vendo operadores unarios

--- tipos-variaveis/src/edu/joas/operadores/Operadores.java
+++ tipos-variaveis/src/edu/joas/operadores/Operadores.java
@@ -1,3 +1,4 @@
+package edu.joas.operadores;
 public class Operadores {
     public static void main(String[] args) {
         // Concatenação de Strings
---
+++ tipos-variaveis/src/edu/joas/operadores/OperadoresUnarios.java
@@ -0,0 +1,14 @@
+package edu.joas.operadores;
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+        
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+}
