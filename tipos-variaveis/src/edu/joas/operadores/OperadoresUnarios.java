e23f28ad653b9bab3f1e5e8c17b9e820ddb03f29
joas-barros <joas.barros14@hotmail.com>
2023-08-27 10:37:06 -0300

    incremento e decremento

--- tipos-variaveis/src/edu/joas/operadores/OperadoresUnarios.java
+++ tipos-variaveis/src/edu/joas/operadores/OperadoresUnarios.java
@@ -10,5 +10,28 @@ public class OperadoresUnarios {
         numero = numero * -1;
 
         System.out.println(numero);
+
+        // Repetição
+        numero = 5;
+
+        numero++;
+        // numero += 1;
+
+        System.out.println(++numero);
+        // incrementa e depois imprime
+
+        System.out.println(numero++);
+        // imprime e depois incrementa
+
+        // TEMBEM SERVE PARA DECREMENTO
+        System.out.println(--numero);
+
+        System.out.println(numero--);
+        
+
+        // Invertendo valores booleanos
+        boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel);
     }
 }
