468ef56ea566e739f0e5de232a2dbf0f4cfa0a00
joas-barros <joas.barros14@hotmail.com>
2023-08-30 20:54:12 -0300

    terminado classe ContaTerminal

--- desafio-sintaxe/conta-bancaria/src/ContaTerminal.java
+++ desafio-sintaxe/conta-bancaria/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.print("Por favor! Digite o número de sua conta: ");
+            int numero = teclado.nextInt();
+
+            System.out.print("Por favor! Digite o número de sua agencia: ");
+            String agencia = teclado.next();
+
+            System.out.print("Por favor! Digite o seu nome: ");
+            String nomeCliente = teclado.next();
+
+            System.out.print("Por favor! Digite seu saldo: ");
+            double saldo = teclado.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+        }
     }
 }
