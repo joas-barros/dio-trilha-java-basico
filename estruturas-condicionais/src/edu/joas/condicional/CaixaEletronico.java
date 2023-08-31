a4fde0580aada46feae39e6e8b9094190cd34453
joas-barros <joas.barros14@hotmail.com>
2023-08-31 09:50:22 -0300

    Condicionais compostas

---
+++ estruturas-condicionais/src/edu/joas/condicional/CaixaEletronico.java
@@ -0,0 +1,18 @@
+package edu.joas.condicional;
+
+// Estrutura simples
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 30.0;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
+        }
+        else
+            System.out.println("Saldo Insuficiente");
+
+    }
+}
