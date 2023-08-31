1059b0c39f3ba02644f446b1c08898206d5065e2
joas-barros <joas.barros14@hotmail.com>
2023-08-31 14:51:39 -0300

    iniciando estruturas de repetição - FOR

---
+++ estruturas-de-controle/src/edu/joas/repeticao/Carneirinho.java
@@ -0,0 +1,11 @@
+package edu.joas.repeticao;
+
+public class Carneirinho {
+    public static void main(String[] args) {
+        for(int caneirinhos = 1; caneirinhos <= 20; caneirinhos++){
+            System.out.println(caneirinhos + " - carneirinhos");
+        }
+
+        System.out.println("zzzzzzzz");
+    }
+}
