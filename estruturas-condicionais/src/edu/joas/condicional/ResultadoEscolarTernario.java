a204d32bfe69dd1d2d419a9ab5e53b1d7d193384
joas-barros <joas.barros14@hotmail.com>
2023-08-31 10:23:40 -0300

    Ternario

---
+++ estruturas-condicionais/src/edu/joas/condicional/ResultadoEscolarTernario.java
@@ -0,0 +1,11 @@
+package edu.joas.condicional;
+
+public class ResultadoEscolarTernario {
+    public static void main(String[] args) {
+        int nota = 5;
+
+		String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+
+		System.out.println(resultado);
+    }
+}
