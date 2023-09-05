1bd12c498dd35f608e17583009071f59a0be64f8
joas-barros <joas.barros14@hotmail.com>
2023-09-05 11:45:41 -0300

    construtores

---
+++ orientacao-a-objetos/src/edu/joas/construtores/SistemaCadastro.java
@@ -0,0 +1,17 @@
+package edu.joas.construtores;
+
+public class SistemaCadastro {
+    public static void main(String[] args) {
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("Marcos", "123", "1113");
+		
+		//definimos o endereço de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		//e como definir o nome e cpf do marcos ?
+		
+		//imprimindo o marcos sem o nome e cpf
+		
+		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+	}
+}
